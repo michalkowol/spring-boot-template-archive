35c9f3ed2889955507ef386c4e2ffc35f700d5e6
Michal Kowol <michal.kowol@vimn.com>
2017-03-13 10:49:54 +0100

    NT Changed title in index.html

--- src/main/java/com/michalkowol/RestTemplateConfiguration.java
+++ src/main/java/com/michalkowol/RestTemplateConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-class RestTemplateComponent {
+class RestTemplateConfiguration {
 
     @Bean
     RestTemplate restTemplate() {
